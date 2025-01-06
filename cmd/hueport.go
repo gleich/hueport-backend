c9496395e32e7dceb1543d8e71329a238040f798
Matt Gleich <git@mattglei.ch>
2025-01-06 18:59:31 -0500

    feat: update log format
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- cmd/hueport.go
+++ cmd/hueport.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gleich/hueport-backend/internal/marketplace"
 	"github.com/gleich/lumber/v3"
 )
 
 func main() {
+	setupLogger()
 	lumber.Info("booted")
 
 	client := http.DefaultClient
@@ -19,4 +21,14 @@ func main() {
 	for i, e := range extensions {
 		lumber.Debug(i, e.DisplayName)
 	}
+
+}
+
+func setupLogger() {
+	nytime, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		lumber.Fatal(err, "failed to load new york timezone")
+	}
+	lumber.SetTimezone(nytime)
+	lumber.SetTimeFormat("01/02 03:04:05 PM MST")
 }
